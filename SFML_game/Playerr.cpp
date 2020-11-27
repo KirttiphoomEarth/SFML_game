a02fe0d7f35aab5033e75c6ba1c2327fa7c78649
earth15032545 <ket15032545@gmail.com>
2020-11-27 17:36:15 +0700

    27-11

--- SFML_game/Playerr.cpp
+++ SFML_game/Playerr.cpp
@@ -6,18 +6,19 @@ Playerr::Playerr(sf::Texture* texture, sf::Vector2u imageCount, float switchtime
 	this->jumpHeight = jumpHeight;
 	row = 0;
 	faceRight = true;
-	
 
-	body.setSize(sf::Vector2f(90.0f, 100.0f));
-	body.setPosition(100, 700.0f);
+
+	body.setSize(sf::Vector2f(60.0f, 70.0f));
+	body.setPosition(100.0f, 200.0f);
 	body.setOrigin(45, 50);
 	body.setTexture(texture);
+	
 }
 
 void Playerr::Update(float deltaTime)
 {
 
-	velocity.x *= 0.5f;
+	velocity.x *= 0.6f;
 	
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
 	{
@@ -29,73 +30,73 @@ void Playerr::Update(float deltaTime)
 		velocity.x += speed;
 	}
 
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::LShift ) and sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::LShift) and sf::Keyboard::isKeyPressed(sf::Keyboard::D))
 	{
-		velocity.x *= 0.9f;
+		velocity.x *= 1.0f;
 		velocity.x += speed;
 	}
 
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::LShift) and sf::Keyboard::isKeyPressed(sf::Keyboard::A))
 	{
-		velocity.x *= 0.9f;
+		velocity.x *= 1.0f;
 		velocity.x -= speed;
 	}
 
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space) && canJump)
 	{
 		canJump = false;
-		velocity.y = -sqrt(2.0f * 981.0f * jumpHeight);
+		velocity.y = -sqrt(1.5f * 981.0f * jumpHeight);
 
 	}
 
 	velocity.y += 981.0f * deltaTime;
-	
-	
+
+
 
 	if (velocity.x == 0.0f)
 	{
 		row = 0;
-		
-	
+
+
 	}
 	else
 	{
 		row = 2;
-		
 
-		if (velocity.x > 0.0f )
+
+		if (velocity.x > 0.0f)
 			faceRight = true;
 		else
 			faceRight = false;
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::J))
 	{
-	
+
 		row = 3;
 	}
-
-	
-	
-		
-	
 	
 
+
 	
+
+
+
+
 
 	Animation.Update(row, deltaTime, faceRight);
 	body.setTextureRect(Animation.uvRect);
 	body.move(velocity * deltaTime);
-	
 
+	printf("x == %.2f   y === %.2f\n", body.getPosition().x, body.getPosition().y);
 
 
 }
 
 void Playerr::Draw(sf::RenderWindow& window)
 {
-	
-		window.draw(body);
-	
+
+	window.draw(body);
+
 
 }
 
@@ -122,4 +123,5 @@ void Playerr::OnCollision(sf::Vector2f direction)
 		//Collision on the top.
 		velocity.y = 0.0f;
 	}
+	
 }
--- SFML_game/main.cpp
+++ SFML_game/main.cpp
@@ -1,12 +1,13 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <iostream>
+#include <cassert>
 #include <vector>
 #include"Playerr.h"
 #include "Platform.h"
 #include "Collider.h"
 
-static const float View_HEIGHT = 720.0f;
+static const float View_HEIGHT = 700.0f;
 bool Retry(false);
 
 void ResizeView(const sf::RenderWindow& window, sf::View& view)
@@ -15,22 +16,15 @@ void ResizeView(const sf::RenderWindow& window, sf::View& view)
 	view.setSize((View_HEIGHT)*aspectRatio, View_HEIGHT);
 }
 
-int main()
+void background()
 {
-	sf::RenderWindow window(sf::VideoMode(1920, 1080), "Game Start!");
-	//sf::RectangleShape background(sf::Vector2f(90.0f, 100.0f));
-	//layer.setFillColor(sf::Color::Blue);
-	//background.setPosition(100.0f, 450.0f);
-	sf::Texture playerTexture;
-	playerTexture.loadFromFile("warrior spritesheet calciumtrice.png");
-
-	sf::Texture backgroundTexture;
+	///////////////////////////////////////////// * BACKGROUND * /////////////////////////////////////////////
+	/*sf::Texture backgroundTexture;
 	sf::Sprite background;
-	sf::Vector2u TextureSize(1080, 720);  //Added to store texture size.
-	sf::Vector2u WindowSize(1920, 1080);   //Added to store window size.
-	sf::View view(sf::Vector2f(0.0f, 0.0f), sf::Vector2f(View_HEIGHT, View_HEIGHT));
-	backgroundTexture.loadFromFile("zzbackground.png");
-	if (!backgroundTexture.loadFromFile("zzbackground.png"))
+	sf::Vector2u TextureSize(2048, 720);  //Added to store texture size.
+	sf::Vector2u WindowSize(1024, 786);   //Added to store window size.
+	backgroundTexture.loadFromFile("zzzzzzmap.png");                                      // 69 - 12 = 57// 57 - 12 = 45
+	if (!backgroundTexture.loadFromFile("zzzzzzmap.png"))
 	{
 		return -1;
 	}
@@ -43,16 +37,28 @@ int main()
 		float ScaleY = (float)WindowSize.y / TextureSize.y;     //Calculate scale.
 
 		background.setTexture(backgroundTexture);
-		background.setScale(ScaleX, ScaleY);      //Set scale.  
-	}
+		background.setScale(ScaleX, ScaleY);      //Set scale.
+	}*/
+	///////////////////////////////////////////// * BACKGROUND * /////////////////////////////////////////////
+}
 
-
-
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(1024, 786), "Game Start!");
+	sf::Texture playerTexture;
+	playerTexture.loadFromFile("warrior spritesheet calciumtrice.png");
 	Playerr player(&playerTexture, sf::Vector2u(10, 10), 0.1f, 100.0f, 200.0f);
+	sf::View view(sf::Vector2f(0.0f, 0.0f), sf::Vector2f(View_HEIGHT, View_HEIGHT));
 
+	sf::Texture tex;
+	sf::Sprite Backgroundack;
+	assert(tex.loadFromFile("zzzzzzmap.png"));
+	Backgroundack.setTexture(tex);
+
+	
 	std::vector<Platform> platforms;
 
-	platforms.push_back(Platform(nullptr, sf::Vector2f(3000.0f, 160.0f), sf::Vector2f(500.0f, 900.0f)));
+	platforms.push_back(Platform(nullptr, sf::Vector2f(3000.0f, 160.0f), sf::Vector2f(500.0f, 635.0f)));
 
 	//Platform platformY(nullptr, sf::Vector2f(200.0f, 3000.0f), sf::Vector2f(1990.0f,1000.0f));
 
@@ -63,8 +69,8 @@ int main()
 	while (window.isOpen())
 	{
 		deltaTime = clock.restart().asSeconds();
-		if (deltaTime > 1.0f / 20.0f)
-			deltaTime = 1.0f / 20.0f;
+		if (deltaTime > 1.0f / 30.0f)
+			deltaTime = 1.0f / 30.0f;
 		sf::Event evnt;
 		while (window.pollEvent(evnt))
 		{
@@ -76,6 +82,8 @@ int main()
 			case sf::Event::Resized:
 				//window size
 				//std::cout<<"NEw window width:"<< evnt.size.width << "New window height" << evnt.size.height << std::endl;
+				
+				ResizeView(window, view);
 				break;
 			
 			}
@@ -84,13 +92,13 @@ int main()
 
 
 		}
-	
+			
 	
 		
 		player.Update(deltaTime);
 		//Collider playerGetCollider = player.GetCollider();
 		view.setCenter(player.GetPosition());
-		Collider playerCollision = player.GetCollider(); // Dammit!!!!
+		Collider playerCollision = player.GetCollider(); 
 		sf::Vector2f direction;
 
 		for (Platform& platform : platforms)
@@ -102,12 +110,12 @@ int main()
 
 				//platformY.GetCollider().CheckCollision(playerGetCollider, direction, 1.0f);
 
-
-	window.clear();
-	window.draw(background);
+	window.clear(sf::Color(40, 37, 56));
+	window.draw(Backgroundack);
+	window.setView(view);
 	player.Draw(window);
-	for (Platform& platform : platforms)
-		 platform.Draw(window);
+	/*for (Platform& platform : platforms)
+		 platform.Draw(window);*/
 	window.display();
 
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Escape)) 
